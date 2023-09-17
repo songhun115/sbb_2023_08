96251a5a11d97ed58856ac7498a70f13155eb360
songhun115 <hoho77113@daum.net>
2023-09-17 19:06:44 +0900

    시í큐리티 허용

---
+++ src/main/java/com/sbs/sbb/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.sbs.sbb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+        ;
+        return http.build();
+    }
+}
